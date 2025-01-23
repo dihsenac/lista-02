3c274b4de87242d04b3b58a6fa05d4337689bf4a
ERIBERTO DIEGO PEREIRA <eriberto.dpereira@senacsp.edu.br>
2025-01-23 16:58:52 -0300

    lista 02

--- while , do ,for/obj/Debug/net8.0/while , do ,for.AssemblyInfo.cs
+++ while , do ,for/obj/Debug/net8.0/while , do ,for.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("while , do ,for")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69139d1255b9351ba725d786e13efcd30d3ced20")]
 [assembly: System.Reflection.AssemblyProductAttribute("while , do ,for")]
 [assembly: System.Reflection.AssemblyTitleAttribute("while , do ,for")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
